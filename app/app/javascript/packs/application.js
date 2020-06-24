4c648bb4cd1322649b39ab2b72dcbde21b255baa
Walid <hello@walidvb.com>
2020-06-24 17:56:31 -0300

    adds react-rails

--- app/app/javascript/packs/application.js
+++ app/app/javascript/packs/application.js
@@ -15,4 +15,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-alert('Webpack ran')
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
