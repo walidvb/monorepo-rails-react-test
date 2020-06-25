93ffed1db952edcf08ab9e096940fa908f89943c
Walid <hello@walidvb.com>
2020-06-25 10:15:49 -0300

    working react import with symlink

--- app/app/javascript/components/HelloWorld.js
+++ app/app/javascript/components/HelloWorld.js
@@ -1,12 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
-import reverser from 'dep-test'
+import reverser from 'dep-test/index'
+import DepComp from 'dep-test/DepComp'
 
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Reversed by dependency: {reverser(this.props.greeting)}
+        {/* <DepComp name={'Rendered by dependency!'}/> */}
       </React.Fragment>
     );
   }
--- app/config/webpack/environment.js
+++ app/config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const path = require('path')
+
+const resolved = {
+  resolve: {
+    alias: {
+      'dep-test$': path.resolve(__dirname, '..', '..', '..', 'dep-test'),
+    },
+    symlinks: false,
+  },
+}
+
+environment.config.merge(resolved)
 
 module.exports = environment
